219fe752e34e2257eec6ec9af4e6b50f93b9a955
rinklepoonia <rinklepoonia889@gmail.com>
2024-08-02 14:25:13 +0530

    pull from dev

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { NAV_LINKS_LIST } from "../common/Helper";
@@ -8,6 +9,8 @@ interface NavLink {
 }
 
 const Header = () => {
+  const [nav, setNav] = useState(true);
+
   return (
     <nav>
       <div className="container">
@@ -21,18 +24,34 @@ const Header = () => {
               className="pointer-events-none"
             />
           </Link>
-          <div className="flex gap-8">
+          <div className="flex flex-col lg:hidden">
+            <span
+              className={`w-[36px] h-[3px] bg-dark_red inline-block rounded`}
+            ></span>
+            <span
+              className={`w-[36px] h-[3px] bg-dark_red inline-block rounded my-2`}
+            ></span>
+            <span
+              className={`w-[36px] h-[3px] bg-dark_red inline-block rounded`}
+            ></span>
+          </div>
+
+          <div
+            className={`${nav && "max-lg:left-[-100%]"} ${
+              !nav && "max-lg:start-0"
+            } flex gap-8 max-lg:bg-white max-lg:flex-col max-lg:items-center max-lg:justify-center max-lg:fixed max-lg:w-full max-lg:h-full max-lg:top-0`}
+          >
             {NAV_LINKS_LIST.map((obj: NavLink, index: number) => (
               <Link
                 key={index}
                 href={obj.href}
-                className="font-jakarta text-black opacity-90 hover:"
+                className="font-jakarta text-black opacity-90 gradient_text transition duration-300 ease-linear"
               >
                 {obj.nav_tabs}
               </Link>
             ))}
           </div>
-          <button className="font-jakarta font-semibold text-dark_red py-[10px] px-8 border border-dark_red rounded">
+          <button className="font-jakarta font-semibold text-dark_red py-[10px] px-8 border border-dark_red rounded hover:bg-dark_red hover:text-white transition duration-300 ease-linear">
             Start Building
           </button>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,8 @@ const config: Config = {
       },
       backgroundImage: {
         mix_red: "linear-gradient(96.09deg, #E31930 0%, #F3576E 100%)",
-        light_red:"linear-gradient(96.09deg, rgba(227, 25, 48, 0.1) 0%, rgba(243, 87, 110, 0.1) 100%)"
+        light_red: "linear-gradient(96.09deg, rgba(227, 25, 48, 0.1) 0%, rgba(243, 87, 110, 0.1) 100%)"
+
       },
     },
   },
