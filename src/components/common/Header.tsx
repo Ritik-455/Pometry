0194d92f544e93f41ea2afcf7a641b2f51f7e469
rinklepoonia <rinklepoonia889@gmail.com>
2024-08-02 13:53:43 +0530

    pull from dev

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { NAV_LINKS_LIST } from "../common/Helper";
+interface NavLink {
+  href: string;
+  nav_tabs: string;
+}
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className="container">
+        <div className="flex items-center justify-between pt-6">
+          <Link href="" aria-label="header-logo">
+            <Image
+              width={157}
+              height={36}
+              src="/assets/images/home/webp/pometry-logo.webp"
+              alt="pometry-logo"
+              className="pointer-events-none"
+            />
+          </Link>
+          <div className="flex gap-8">
+            {NAV_LINKS_LIST.map((obj: NavLink, index: number) => (
+              <Link
+                key={index}
+                href={obj.href}
+                className="font-jakarta text-black opacity-90 hover:"
+              >
+                {obj.nav_tabs}
+              </Link>
+            ))}
+          </div>
+          <button className="font-jakarta font-semibold text-dark_red py-[10px] px-8 border border-dark_red rounded">
+            Start Building
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/components/common/Helper.tsx
@@ -0,0 +1,7 @@
+// ===header
+export const NAV_LINKS_LIST = [
+  { nav_tabs: "Home", href: "#home" },
+  { nav_tabs: "Blog", href: "#blog" },
+  { nav_tabs: "Our Team", href: "#team" },
+  { nav_tabs: "Product Overview", href: "#overview" },
+];
