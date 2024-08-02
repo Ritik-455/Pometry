6f3c56fbe8484d2c03d72b7956b208acb197fe85
rinklepoonia <rinklepoonia889@gmail.com>
2024-08-02 12:15:56 +0530

    common-heading

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -8,7 +8,7 @@ const CommonButton = ({ className, textName }: BUTTON) => {
   return (
     <div>
       <button
-        className={`font-jakarta font-semibold text-white rounded border border-transparent py-3 px-[30px] bg-mix_red ${className}`}
+        className={`font-jakarta font-semibold text-white rounded border border-transparent py-3 px-[30px] bg-mix_red hover:bg-transparent hover:border-light_red hover:text-light_red ${className}`}
       >
         {textName}
       </button>
