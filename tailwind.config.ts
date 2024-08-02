4c2704ffc84b4a3aadcaf4cd47910fb83b7aeb88
Ritik <rvjaat25@gmail.com>
2024-08-02 11:29:00 +0530

    config-file-added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "jakarta ": "'Plus Jakarta Sans',sans-serif",
+        "courier": "'Courier Prime',monospace"
+      },
+      colors: {
+        black: "#111111",
+        dark_red: "#9D2035",
+        light_black: "#FFFFFF1A",
+        green: "#00c136",
+        yellow: "#EDF127",
+        light_red: "#9D20351A"
+      },
+      container: {
+        padding: {
+          DEFAULT: "20px",
+          sm: "24px",
+          xl: "20px",
+        },
+        center: true,
+      }
+    },
   },
   plugins: [],
 };
