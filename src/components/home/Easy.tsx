f09f0ad135ab4e48de4fc96ae538a95ad934bee2
Ritik <rvjaat25@gmail.com>
2024-08-03 12:00:46 +0530

    section-completed

--- src/components/home/Easy.tsx
+++ src/components/home/Easy.tsx
@@ -20,39 +20,55 @@ const Easy = () => {
   };
   return (
     <div className="bg-jet_black">
-      <div className="container max-w-[1180px] pt-[124px] pb-[101px]">
+      <div className="container max-w-[1180px] xl:pt-[124px] py-16 md:py-20 xl:pb-[101px]">
         <PrimaryHeading textName="As Easy As" className="!text-white text-center" />
         <div className="flex justify-center">
           <Paragraph textName="Ingest millions of rows per second from your relational database, CSV files, cloud storage or big data warehouses. No need to move or transform your data, simply point to your datasource!" className="!text-white opacity-80 max-w-[756px] text-center mt-3" />
         </div>
-        <div className="row mt-14">
+        <div className="hidden md:block">
+          <div className="row mt-14">
+            {EASY_TABS_DATA.map((data: Easy_Tab, index) => (
+              <div className={`${index === activeTab ? ' md:border-dark_red' : ' md:border-[#2E2E2E]'} col-4 md:border-l-[2px] min-h-[180px] lg:min-h-[215px] cursor-pointer`} key={index} onClick={() => handleTabClick(index)}>
+                <div className="lg:pl-6 pt-6">
+                  <div className={`${index === activeTab ? 'bg-mix_red' : 'bg-light_black'} size-11 rounded-lg flex justify-center items-center`}>
+                    <p className=" font-jakarta font-semibold text-2xl text-white">{data.numb}</p>
+                  </div>
+                  <p className={`${index === activeTab ? 'opacity-100' : 'opacity-80'} font-jakarta font-semibold text-base lg:text-xl text-white opacity-80 mt-4`}>{data.heading}</p>
+                  <Paragraph textName={data.para} className=" mt-2 !text-white max-w-[316px] opacity-80 text-sm lg:text-base font-normal" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex md:hidden overflow-x-scroll gap-5 mt-6">
           {EASY_TABS_DATA.map((data: Easy_Tab, index) => (
-            <div className={`${index === activeTab ? ' md:border-dark_red' : ' md:border-[#2E2E2E]'} col-4 md:border-l-[2px] min-h-[215px] cursor-pointer`} key={index} onClick={() => handleTabClick(index)}>
+            <div className={`${index === activeTab ? ' md:border-dark_red' : ' md:border-[#2E2E2E]'} md:border-l-[2px] min-h-[180px] cursor-pointer`} key={index} onClick={() => handleTabClick(index)}>
               <div className="lg:pl-6 pt-6">
                 <div className={`${index === activeTab ? 'bg-mix_red' : 'bg-light_black'} size-11 rounded-lg flex justify-center items-center`}>
                   <p className=" font-jakarta font-semibold text-2xl text-white">{data.numb}</p>
                 </div>
-                <p className={`${index === activeTab ? 'opacity-100' : 'opacity-80'} font-jakarta font-semibold text-xl text-white opacity-80 mt-4`}>{data.heading}</p>
-                <Paragraph textName={data.para} className=" mt-2 !text-white max-w-[316px] opacity-80" />
+                <p className={`${index === activeTab ? 'opacity-100' : 'opacity-80'} font-jakarta font-semibold text-base lg:text-xl text-white opacity-80 mt-4`}>{data.heading}</p>
+                <Paragraph textName={data.para} className=" mt-2 !text-white w-[280px] opacity-80 text-sm lg:text-base font-normal" />
               </div>
             </div>
           ))}
+
         </div>
         {/* tab-1 content */}
         {activeTab === 0 && (
           <div className="">
             <div className="flex justify-center mt-10">
               <div className="max-w-[640px] flex justify-between w-full">
-                <div className="size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
+                <div className="size-16 sm:size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
                   <Document />
                 </div>
-                <div className="size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
+                <div className="size-16 sm:size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
                   <CloudCoin />
                 </div>
-                <div className="size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
+                <div className="size-16 sm:size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
                   <Star />
                 </div>
-                <div className="size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
+                <div className="size-16 sm:size-[82px] bg-light_red rounded-[20px] flex justify-center items-center">
                   <Coin />
                 </div>
               </div>
@@ -64,7 +80,7 @@ const Easy = () => {
                 width={640}
                 alt="Run"
               />
-              <div className="max-w-[596px] w-full bg-light_black p-[35px_43px_24px_43px] border border-[#FFFFFF1F] rounded-[15px]">
+              <div className="max-w-[596px] w-full bg-light_black p-4 sm:p-[35px_43px_24px_43px] border border-[#FFFFFF1F] rounded-[15px]">
                 <p className="text-xl font-jakarta text-white">Run Queries / Analysis</p>
                 <p className="text-sm text-white font-courier mt-6 font-normal">Graph.<span className="text-green">.rangeQuery &#40;</span></p>
                 <p className="text-sm text-white font-courier mt-1 font-normal">Algorithm = AnomalyDetetioon(),</p>
@@ -80,28 +96,40 @@ const Easy = () => {
         {/* tab-2 content */}
         {activeTab === 1 && (
           <div className="">
-            <div className="flex gap-4 mt-10">
-              <div className="max-w-[558px] w-full bg-light_black p-4 border border-[#FFFFFF1F] rounded-[15px]">
-                <p className="font-courier font-normal text-white text-sm">for monthly_view in graph<span className="text-green">.rolling</span>( &#34;<span className="text-yellow">1 month</span> &#34;) :</p>
-                <p className="font-courier font-normal text-white text-sm mt-2">EntityResolution() -&#62; Scoring() -&#62; Alerting()</p>
-                <p className="font-courier font-normal text-white text-sm mt-2">Save_to_Postgres()</p>
+            <div className="flex gap-4 mt-10 flex-wrap lg:flex-nowrap justify-center">
+              <div className="w-[540px] lg:w-[558px]">
+                <div className="max-w-[558px] w-full bg-light_black p-4 border border-[#FFFFFF1F] rounded-[15px] lg:h-full">
+                  <p className="font-courier font-normal text-white text-sm">for monthly_view in graph<span className="text-green">.rolling</span>( &#34;<span className="text-yellow">1 month</span> &#34;) :</p>
+                  <p className="font-courier font-normal text-white text-sm mt-2">EntityResolution() -&#62; Scoring() -&#62; Alerting()</p>
+                  <p className="font-courier font-normal text-white text-sm mt-2">Save_to_Postgres()</p>
+                </div>
+                <div className="max-w-[558px] w-full mt-4 lg:hidden">
+                  <p className="font-jakarta font-semibold text-lg text-white w-full">Intuitive GraphQL end-points</p>
+                  <p className="font-jakarta text-sm font-normal text-white max-w-[420px] mt-2">Explore your data using our intuitive GraphQL APIs and integrate with yourfront-end solutions within minutes</p>
+                </div>
               </div>
-              <div className="max-w-[558px] w-full bg-light_black p-4 pb-0 border border-[#FFFFFF1F] rounded-[15px]">
-                <p className="font-courier font-normal text-white text-base">window(start: &#34;<span className="text-yellow">01/01/2023</span>&#34;, end: &#34;<span className="text-yellow">01/06/2023</span>&#34;)  &#123;</p>
-                <p className="font-courier font-normal text-white text-base ml-3">nodes(type: &#34;<span className="text-yellow">company</span>&#34;) &#123;</p>
-                <p className="font-courier font-normal text-white text-base ml-5">company_number, incorporation_date,</p>
-                <p className="font-courier font-normal text-white text-base ml-5"><span className="text-green">neighbours</span>(type: &#34;<span className="text-yellow">directo</span>r&#34;) &#123;</p>
-                <p className="font-courier font-normal text-white text-base ml-11">name, age, address</p>
-                <p className="font-courier font-normal text-white text-base ml-3">&#125;</p>
-                <p className="font-courier font-normal text-white text-base">&#125; </p>
-                <p className="font-courier font-normal text-white text-base"><span className="text-green">algorithms</span> &#123;</p>
-                <p className="font-courier font-normal text-white text-base ml-5">shell_detection &#44;</p>
-                <p className="font-courier font-normal text-white text-base ml-5">pep_screening</p>
-                <p className="font-courier font-normal text-white text-base ml-3">&#125;</p>
-                <p className="font-courier font-normal text-white text-base">&#125; </p>
+              <div className="w-[558px]">
+                <div className="max-w-[558px] w-full bg-light_black p-4 pb-0 border border-[#FFFFFF1F] rounded-[15px]">
+                  <p className="font-courier font-normal text-white text-base">window(start: &#34;<span className="text-yellow">01/01/2023</span>&#34;, end: &#34;<span className="text-yellow">01/06/2023</span>&#34;)  &#123;</p>
+                  <p className="font-courier font-normal text-white text-base ml-3">nodes(type: &#34;<span className="text-yellow">company</span>&#34;) &#123;</p>
+                  <p className="font-courier font-normal text-white text-base ml-5">company_number, incorporation_date,</p>
+                  <p className="font-courier font-normal text-white text-base ml-5"><span className="text-green">neighbours</span>(type: &#34;<span className="text-yellow">directo</span>r&#34;) &#123;</p>
+                  <p className="font-courier font-normal text-white text-base ml-11">name, age, address</p>
+                  <p className="font-courier font-normal text-white text-base ml-3">&#125;</p>
+                  <p className="font-courier font-normal text-white text-base">&#125; </p>
+                  <p className="font-courier font-normal text-white text-base"><span className="text-green">algorithms</span> &#123;</p>
+                  <p className="font-courier font-normal text-white text-base ml-5">shell_detection &#44;</p>
+                  <p className="font-courier font-normal text-white text-base ml-5">pep_screening</p>
+                  <p className="font-courier font-normal text-white text-base ml-3">&#125;</p>
+                  <p className="font-courier font-normal text-white text-base">&#125; </p>
+                </div>
+                <div className="max-w-[558px] w-full mt-4 lg:hidden">
+                  <p className="font-jakarta font-semibold text-lg text-white w-full">Go beyond SQL with native Python support</p>
+                  <p className="font-jakarta text-sm font-normal text-white max-w-[495px] mt-2">Run complex graph queries across time, extract machine learning featuresand feedback to your existing pipelines natively</p>
+                </div>
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="hidden lg:flex justify-between">
               <div className="max-w-[558px] w-full mt-6">
                 <p className="font-jakarta font-semibold text-xl text-white w-full">Intuitive GraphQL end-points</p>
                 <p className="font-jakarta text-base font-normal text-white max-w-[420px] mt-3">Explore your data using our intuitive GraphQL APIs and integrate with yourfront-end solutions within minutes</p>
@@ -126,7 +154,7 @@ const Easy = () => {
           </div>
         )}
       </div>
-    </div >
+    </div>
   )
 };
 
