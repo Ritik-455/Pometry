b9cfb53c05e6c19a3a98cd63997e65aba389eac3
rinklepoonia <rinklepoonia889@gmail.com>
2024-08-05 14:40:05 +0530

    transition

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -7,7 +7,7 @@ interface Button {
 const CommonButton = ({ className, textName }: Button) => {
   return (
     <button
-      className={`font-jakarta font-semibold text-white rounded border border-transparent py-3 px-[30px] bg-mix_red hover:bg-none hover:border-dark_red hover:text-dark_red transition duration-300 ease-linear ${className}`}
+      className={`font-jakarta font-semibold text-white rounded border border-transparent py-3 px-[30px] bg-mix_red hover:bg-none hover:border-dark_red hover:text-dark_red transition-all duration-300 ease-linear ${className}`}
     >
       {textName}
     </button>
