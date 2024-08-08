dd8320099b11241fd1a2e3bb7a604faf5cfc41be
rinklepoonia <rinklepoonia889@gmail.com>
2024-08-08 10:01:36 +0530

    deploying

--- src/components/ourteam/MeetOurTeam.tsx
+++ src/components/ourteam/MeetOurTeam.tsx
@@ -1,49 +1,90 @@
+"use client";
+import React, { useState } from "react";
 import PrimaryHeading from "../common/PrimaryHeading";
 import Image from "next/image";
-import { MEET_OUR_TEAM } from "../common/Helper";
+import { MEET_OUR_TEAM, OUR_TEAM_IMAGES } from "../common/Helper";
 import Paragraph from "../common/Paragraph";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Pagination } from "swiper/modules";
+import "swiper/css/pagination";
 
 interface MeetOurTeam {
-  // img: string;
   para: string;
   name: string;
   profession: string;
 }
+interface TeamImages {
+  img: string;
+}
 const MeetOurTeam = () => {
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
+
   return (
     <div className="pt-[152px] pb-[124px]">
-      <div className="container max-w-[1180px]">
         <PrimaryHeading
           className="text-center pb-[52px]"
           textName="Meet Our Team"
         />
-        {MEET_OUR_TEAM.map((obj: MeetOurTeam, index: number) => (
-          <div key={index}>
-            {/* <Image
-              src={obj.img}
-              width={259}
-              height={259}
-              alt="meet-our-team-slider-img"
-              className="pointer-events-none"
-            /> */}
-            <div className="shadow-8xl py-[30px] px-12 rounded-2xl">
-              <Paragraph
-                className="max-w-[980px] text-center !text-charcoal_gray pb-6"
-                textName={obj.para}
+        <Swiper
+          spaceBetween={16}
+          slidesPerView={3}
+          loop={true}
+        >
+          {OUR_TEAM_IMAGES.map((obj: TeamImages, index: number) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={obj.img}
+                width={259}
+                height={259}
+                alt="meet-our-team-slider-img"
+                className="pointer-events-none"
               />
-              <p className="font-semibold text-[32px] leading-10 text-dark_red text-center pb-0.5">
-                {obj.name}
-              </p>
-              <Paragraph
-                className="!text-charcoal_gray text-center"
-                textName={obj.profession}
-              />
-            </div>
-          </div>
-        ))}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          spaceBetween={16}
+          slidesPerView={1}
+          loop={true}
+          pagination={true}
+          modules={[Pagination]}
+        >
+          {/* =====team-content===== */}
+          {MEET_OUR_TEAM.map((obj: MeetOurTeam, index: number) => (
+            <SwiperSlide key={index}>
+              <div className="shadow-8xl py-[30px] px-12 rounded-2xl">
+                <Paragraph
+                  className="max-w-[980px] text-center !text-charcoal_gray pb-6"
+                  textName={obj.para}
+                />
+                <p className="font-semibold text-[32px] leading-10 text-dark_red text-center pb-0.5">
+                  {obj.name}
+                </p>
+                <Paragraph
+                  className="!text-charcoal_gray text-center"
+                  textName={obj.profession}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-    </div>
   );
 };
 
 export default MeetOurTeam;
+//  {
+//    OUR_TEAM_IMAGES.map((obj: TeamImages, index: number) => (
+//      <SwiperSlide key={index}>
+//        <Image
+//          src={obj.img}
+//          width={259}
+//          height={259}
+//          alt="meet-our-team-slider-img"
+//          className="pointer-events-none"
+//        />
+//      </SwiperSlide>
+//    ));
+//  }
